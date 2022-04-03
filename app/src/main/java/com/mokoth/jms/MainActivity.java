fe850a86f57053b624c8942f9f6a3dfd1720e213
moses okoth <jackmokn@gmail.com>
2022-04-03 11:53:38 +0300

    Adding grid view

--- app/src/main/java/com/mokoth/jms/MainActivity.java
+++ app/src/main/java/com/mokoth/jms/MainActivity.java
@@ -3,12 +3,29 @@ package com.mokoth.jms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    GridView coursesGV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        coursesGV = findViewById(R.id.idGVcourses);
+
+        ArrayList<CourseModel> courseModelArrayList = new ArrayList<CourseModel>();
+        courseModelArrayList.add(new CourseModel("C#", R.drawable.ic_launcher_foreground));
+        courseModelArrayList.add(new CourseModel("JAVA", R.drawable.ic_launcher_foreground));
+        courseModelArrayList.add(new CourseModel("C++", R.drawable.ic_launcher_foreground));
+        courseModelArrayList.add(new CourseModel("Python", R.drawable.ic_launcher_foreground));
+        courseModelArrayList.add(new CourseModel("Javascript", R.drawable.ic_launcher_foreground));
+        courseModelArrayList.add(new CourseModel("DSA", R.drawable.ic_launcher_foreground));
+
+        CourseGVAdapter adapter = new CourseGVAdapter(this, courseModelArrayList);
+        coursesGV.setAdapter(adapter);
     }
 }
